f2c3269fe7ac08ba5db932081de02408f37eb9ba
Matthew Early <matthewjearly@gmail.com>
2021-12-03 15:08:30 -0500

    use imgui bindings from vcpkg

--- AAEngine/src/GUI/imGUI.h
+++ AAEngine/src/GUI/imGUI.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <imgui.h>
-#include "../vendor/imgui/imgui_impl_glfw.h"
-#include "../vendor/imgui/imgui_impl_opengl3.h"
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
 namespace AA {
 class imGUI {
 public:
