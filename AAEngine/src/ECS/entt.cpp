c77f230a6d3e19ada530051d5c9694acf0d4f43f
Matthew Early <matthewjearly@gmail.com>
2021-12-14 01:12:51 -0500

    add base singleton entt version

--- AAEngine/src/ECS/entt.cpp
+++ AAEngine/src/ECS/entt.cpp
@@ -1,4 +1,10 @@
 #include "entt.h"
 namespace AA {
-
+static Entt* entt_instance;
+entt::basic_registry<entity_id>* Entt::Get() {
+  if (!entt_instance) {
+    entt_instance = new Entt();
+  }
+  return &entt_instance->mRegistry;
+}
 }
--- AAEngine/src/ECS/entt.h
+++ AAEngine/src/ECS/entt.h
@@ -1,9 +1,33 @@
 #pragma once
-//#include <entt
+#include <entt/entt.hpp>
 namespace AA {
-class Entt {
+
+struct entity_id {
+  using entity_type = std::uint32_t;
+  static constexpr auto null = entt::null;
+
+  constexpr entity_id(entity_type value = null) ENTT_NOEXCEPT
+    : entt{ value } {}
+
+  constexpr entity_id(const entity_id& other) ENTT_NOEXCEPT
+    : entt{ other.entt } {}
+
+  constexpr operator entity_type() const ENTT_NOEXCEPT {
+    return entt;
+  }
+
 private:
-  //entt::registry mRegistry;
+  entity_type entt;
+};
+
+class Entt { 
+  //singleton
+  static entt::basic_registry<entity_id>* Get();
+
+private:
+  Entt();
+
+  entt::basic_registry<entity_id> mRegistry;
 
 };
 }
