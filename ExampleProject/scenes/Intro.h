336bb80f61dd322dfd8670a3c889e3f17a6a6fad
Matthew Early <matthewjearly@gmail.com>
2021-12-20 05:30:23 -0500

    intro/menu flow change

--- ExampleProject/scenes/Intro.h
+++ ExampleProject/scenes/Intro.h
@@ -7,12 +7,12 @@ static bool intro_loaded = false;
 static const char* intro_music_path = "res/acoustic_intro_dec_18_2021.ogg";
 static const char* intro_logo_path = "res/neon_cube.glb";
 static int intro_logo_id = -1;
-static int cam_id = -1;
+static int intro_cam_id = -1;
 
 void LoadIntro() {
   Engine.AddMusic(intro_music_path);
-  cam_id = Engine.AddCamera();
-  Engine.SetCamPosition(cam_id, glm::vec3(0,0,40));
+  intro_cam_id = Engine.AddCamera(Engine.GetWindowWidth(), Engine.GetWindowHeight());
+  Engine.SetCamPosition(intro_cam_id, glm::vec3(0,0,40));
   intro_logo_id = Engine.AddProp(intro_logo_path);
   intro_loaded = true;
   Engine.PlayMusic();
@@ -21,7 +21,7 @@ void LoadIntro() {
 void TickIntro(const float& dt) {
   if (intro_logo_id != -1) {
     static float curr_rot = 0.f;
-    curr_rot += dt * 1.5;
+    curr_rot += dt * 1.5f;
     Engine.RotateProp(intro_logo_id, glm::vec3(0, curr_rot, 0));
   }
 }
@@ -34,7 +34,7 @@ void UnloadIntro() {
   Engine.RemoveMusic();
   Engine.RemoveProp(intro_logo_id);
   intro_logo_id = -1;
-  Engine.RemoveCamera(cam_id);
-  cam_id = -1;
+  Engine.RemoveCamera(intro_cam_id);
+  intro_cam_id = -1;
   intro_loaded = false;
 }
--- ExampleProject/scenes/Menu.h
+++ ExampleProject/scenes/Menu.h
@@ -7,11 +7,12 @@ static bool menu_loaded = false;
 static const char* menu_music_path = "res/acoustic_menu_dec_19_2021.ogg";
 static const char* menu_logo_path = "res/neon_sphere.glb";
 static int menu_logo_id = -1;
+static int menu_cam_id = -1;
 
 void LoadMenu() {
   Engine.AddMusic(menu_music_path);
-  auto camid = Engine.AddCamera();
-  Engine.SetCamPosition(camid, glm::vec3(0,0,40));
+  menu_cam_id = Engine.AddCamera(Engine.GetWindowWidth(), Engine.GetWindowHeight());
+  Engine.SetCamPosition(menu_cam_id, glm::vec3(0, 0, 40));
   menu_logo_id = Engine.AddProp(menu_logo_path);
   menu_loaded = true;
   Engine.PlayMusic();
@@ -19,9 +20,18 @@ void LoadMenu() {
 
 void TickMenu(const float& dt) {
   if (menu_logo_id != -1) {
-    static float curr_rot = 0.f;
-    curr_rot += dt * 1.5;
-    Engine.RotateProp(menu_logo_id, glm::vec3(0, curr_rot, 0));
+    static float curr_scale = 1.f;
+    static float grow = true;
+    if (grow) {
+      curr_scale += dt;
+      if (curr_scale > 10.f)
+        grow = false;
+    } else {
+      curr_scale -= dt;
+      if (curr_scale < 1.f)
+        grow = true;
+    }
+    Engine.ScaleProp(menu_logo_id, glm::vec3(curr_scale));
   }
 }
 
@@ -29,6 +39,11 @@ void UnloadMenu() {
   if (!menu_loaded) {
     return;
   }
+  Engine.StopMusic();
   Engine.RemoveMusic();
+  Engine.RemoveCamera(menu_cam_id);
+  menu_cam_id = -1;
+  Engine.RemoveProp(menu_logo_id);
+  menu_logo_id = -1;
   menu_loaded = false;
 }
