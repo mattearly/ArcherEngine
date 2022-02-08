279b8bd06b18bd9603d3e27d9fc9363b5583ab11
Matthew Early <matthewjearly@gmail.com>
2022-02-08 16:40:26 -0500

    add class notes

--- AAEngine/src/Scene/Skybox.h
+++ AAEngine/src/Scene/Skybox.h
@@ -6,6 +6,11 @@
 #include <string>
 #include <memory>
 namespace AA {
+
+// sticking with this pattern: 
+// https://learnopengl.com/Advanced-OpenGL/Cubemaps
+// although many skybox texture sets found online seem to be in differing order
+// you may need to adapt accordingly.
 class Skybox {
 public:
   Skybox(std::vector<std::string> incomingSkymapFiles);
