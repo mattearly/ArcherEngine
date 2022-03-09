7d5fc56b1ea395730e66ebf54685f36d9fc44b19
Matthew Early <matthewjearly@gmail.com>
2022-03-09 09:04:16 -0500

    remove example proj

--- ExampleProject/GameStates.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-enum class STATE { OFF, PLAY_SCENE };
-enum class SCENE { INTRO, MAIN_MENU, CHARACTER, LEVEL1 };
--- ExampleProject/elements/Loadout.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "Item.h"
-
-struct Loadout {
-  Item main_hand;
-  Item off_hand;
-  Item head, neck, mid, back, arms, hands, waist, legs, feet;
-};
