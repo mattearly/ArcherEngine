20b7c0f2192d8f7ba671cbe1bba932425b0ebbc4
Matthew Early <matthewjearly@gmail.com>
2022-04-11 13:12:54 -0400

    reset debug light indicators on teardown

--- AAEngine/src/IntefaceRunetime.cpp
+++ AAEngine/src/IntefaceRunetime.cpp
@@ -172,6 +172,7 @@ void Interface::teardown() {
   mDirectionalLight.reset();
   mPointLights.clear();
   mSpotLights.clear();
+  mDebugLightIndicators = false;
 
   // delete imgui
   if (mIMGUI) {
