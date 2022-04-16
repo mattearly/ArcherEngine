b2adfe84121fbffb894c6335ea27f393bc31d2e2
Matthew Early <matthewjearly@gmail.com>
2022-04-16 16:27:14 -0400

    fix Normal calculation

--- AAEngine/src/OS/OpenGL/InternalShaders/Uber.cpp
+++ AAEngine/src/OS/OpenGL/InternalShaders/Uber.cpp
@@ -40,22 +40,21 @@ void main(){
   vec4 totalPosition = vec4(0.0);
 
   if (u_is_animating > 0) {
-    vec3 totalNormal = vec3(0.0);
     for(int i = 0 ; i < MAX_BONE_INFLUENCE ; i++) {
       if(inBoneIds[i] == -1) continue;
       if(inBoneIds[i] >= MAX_BONES) {
           totalPosition = vec4(inPos, 1.0f);
           break;
       }
-      vec4 localPosition = u_final_bone_mats[inBoneIds[i]] * vec4(inPos,1.0);
+      vec4 localPosition = u_final_bone_mats[inBoneIds[i]] * vec4(inPos, 1.0);
       totalPosition += localPosition * inWeights[i];
-      //totalNormal += mat3(u_final_bone_mats[inBoneIds[i]]) * inNorm;
     }
     vs_out.Pos = (u_model_matrix * totalPosition).xyz;
+    vs_out.Norm = inNorm;
   } else {  // Not Animating
     mat3 normal_matrix = transpose(inverse(mat3(u_model_matrix)));
-    vs_out.Norm = normalize(normal_matrix * inNorm);
     vs_out.Pos = (u_model_matrix * vec4(inPos, 1.0)).xyz;
+    vs_out.Norm = normalize(normal_matrix * inNorm);
     totalPosition = vec4(inPos, 1.0);
   }
   mat4 viewMatrix = u_view_matrix * u_model_matrix;
