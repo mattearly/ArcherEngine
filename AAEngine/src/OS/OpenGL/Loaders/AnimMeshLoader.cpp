dbb5983a154fea6a1b1fcba0089d4f59d0c59bf1
Matthew Early <matthewjearly@gmail.com>
2022-05-03 13:41:36 -0400

    resolve include path

--- AAEngine/src/OS/OpenGL/Loaders/AnimMeshLoader.cpp
+++ AAEngine/src/OS/OpenGL/Loaders/AnimMeshLoader.cpp
@@ -1,7 +1,7 @@
 #include "AnimMeshLoader.h"
 #include "TextureLoader.h"
-#include "OpenGL/Graphics.h"
-#include "../../include/AAEngine/Mesh/AnimProp.h"
+#include "../Graphics.h"
+#include "../../../../include/AAEngine/Mesh/AnimProp.h"
 #include <assimp/Importer.hpp>
 #include <assimp/postprocess.h>
 
