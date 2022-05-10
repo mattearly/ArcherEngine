d22b3e0ec10146030f982654b3d0e317d6816235
Matthew Early <matthewjearly@gmail.com>
2022-05-10 18:03:54 -0400

    init materials

---
+++ AAEngine/src/Mesh/Material.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <vector>
+namespace AA {
+struct Material {
+public:
+  ~Material();
+  glm::vec3 Ambient{};
+  glm::vec3 Diffuse{};
+  glm::vec3 Specular{};
+  glm::vec3 Emission{};
+  float SpecularHightlights{};  // 0 to 1000   - aka shininess
+  float OpticalDensity{};       // 0.001 to 10 - aka refraction index
+  float Dissolve{};             // 1.0 to 0    - aka alpha color
+  int IlluminationModel{};      // ignore
+  // texture maps to blend with  - data not saved, see TextureInfo.h
+};
+}
+
