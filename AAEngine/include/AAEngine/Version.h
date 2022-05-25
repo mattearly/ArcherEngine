2bb864f4af566259ce22bed866a2b4837e1b46e2
Matthew Early <matthewjearly@gmail.com>
2022-05-24 20:33:54 -0400

    v1.3->v1.4

--- AAEngine/include/AAEngine/Version.h
+++ AAEngine/include/AAEngine/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 namespace AA {
 constexpr unsigned int ENGINEVERSIONMAJOR = 1;
-constexpr unsigned int ENGINEVERSIONMINOR = 3;
+constexpr unsigned int ENGINEVERSIONMINOR = 4;
 constexpr unsigned int ENGINEVERSIONPATCH = 0;
 }
