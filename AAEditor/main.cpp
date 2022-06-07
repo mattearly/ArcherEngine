077685d2fa67126af675cbaa69b7cef8a9b399f8
Matthew Early <matthewjearly@gmail.com>
2022-06-07 16:44:46 -0400

    formatting

--- AAEditor/main.cpp
+++ AAEditor/main.cpp
@@ -16,18 +16,15 @@ int main() {
 
   editor_cam = engine.AddCamera();
   {  // default cam settings
-    auto weak = engine.GetCamera(editor_cam);
-    auto strong = weak.lock();
-    strong->SetPosition(glm::vec3(0.0f, 40.0f, -40.f));
-    strong->SetYaw(90.f);
-    strong->SetPitch(-35.f);
+    auto strong_cam_ref = engine.GetCamera(editor_cam).lock();
+    strong_cam_ref->SetPosition(glm::vec3(0.0f, 65.0f, -65.f));
+    strong_cam_ref->SetYaw(90.f);
+    strong_cam_ref->SetPitch(-35.f);
 
     { // default sun and shadows settings
-      auto weakd = engine.GetSunLight();
-      auto strongd = weakd.lock();
-      strongd->ShadowOrthoSize = (strong->GetRenderDistance()+200.f)/2.f;
-      strongd->ShadowFarPlane = strong->GetRenderDistance()+200.f;
-
+      auto strong_sunlight_ref = engine.GetSunLight().lock();
+      strong_sunlight_ref->ShadowOrthoSize = (strong_cam_ref->GetRenderDistance() + 200.f) / 2.f;
+      strong_sunlight_ref->ShadowFarPlane = strong_cam_ref->GetRenderDistance()+200.f;
     }
   }
   setup_fpp_fly(editor_cam, engine);
