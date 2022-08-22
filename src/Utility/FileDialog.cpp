a786ef67dcb17705bfc58553979811e093ffbaf9
Matthew Early <matthewjearly@gmail.com>
2022-08-21 22:09:50 -0400

    remove console output

--- src/Utility/FileDialog.cpp
+++ src/Utility/FileDialog.cpp
@@ -1,12 +1,8 @@
 #include <Utility/FileDialog.h>
-
-
-
 #ifdef __linux__
 #include <stdio.h>
 bool AA::NavigateFileSystem(std::string& out, const char* filetypereg, const char* starting_pref)
 {
-  printf("opening file...");
   out.clear();
   bool func_result = true;
   const int PATH_MAX = 1024;
@@ -18,27 +14,21 @@ bool AA::NavigateFileSystem(std::string& out, const char* filetypereg, const cha
   if (fp == NULL) {
     func_result = false;
     /* Handle error */
-    printf("path failure: %s", path);
   } else {
     fgets(path, PATH_MAX, fp);
     std::string string_ver = path;
     auto end_pos = string_ver.find_first_of('\n');
     out = string_ver.substr(0, end_pos);
-
   }
   status = pclose(fp);
   if (status == -1) {
     /* Error reported by pclose() */
-     printf("error by pclose() %s", path);
   } else {
     /* Use macros described under wait() to inspect `status' in order
        to determine success/failure of command executed by popen() */
   }
-  printf("out path is: %s", out);
   return func_result;
 }
-
-
 
 #elif _WIN32
 #include <nfd.h>
@@ -59,10 +49,4 @@ bool AA::NavigateFileSystem(std::string& out, const char* filetypereg, const cha
   }
   return func_result;
 }
-
-
-
 #endif
-
-
-
