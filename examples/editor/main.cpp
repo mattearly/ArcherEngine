568970da7382bfdd1849d58bcbb8097bd57037b8
Matthew Early <matthewjearly@gmail.com>
2022-11-08 09:46:33 -0500

    add loaded model id list

--- examples/editor/main.cpp
+++ examples/editor/main.cpp
@@ -3,7 +3,7 @@
 
 #include "fpp_fly.h"
 #include "file_menu.h"
-static AA::Interface engine{};
+static AA::Interface archer_engine_interface{};
 static unsigned int editor_cam{};
 int main() {
 
@@ -15,21 +15,21 @@ int main() {
     winopts._windowing_mode = AA::WINDOW_MODE::MAXIMIZED;
     winopts._editor_drag_and_drop = true;
     winopts._title = "AA Editor";
-    if (!engine.Init(winopts)) return -1;
-    engine.SetLogStream(true, false);  // log to console
+    if (!archer_engine_interface.Init(winopts)) return -1;
+    archer_engine_interface.SetLogStream(true, false);  // log to console
 
-    editor_cam = engine.AddCamera();  // add camera
-    engine.SetSunLight(glm::vec3(-.33f, -1.f, -.33f), glm::vec3(.8f), glm::vec3(0.1f), glm::vec3(0.5f));
-    auto strong_cam_ref = engine.GetCamera(editor_cam).lock();
+    editor_cam = archer_engine_interface.AddCamera();  // add camera
+    archer_engine_interface.SetSunLight(glm::vec3(-.33f, -1.f, -.33f), glm::vec3(.8f), glm::vec3(0.1f), glm::vec3(0.5f));
+    auto strong_cam_ref = archer_engine_interface.GetCamera(editor_cam).lock();
     strong_cam_ref->SetPosition(glm::vec3(0.0f, 65.0f, -65.f));
     strong_cam_ref->SetYaw(90.f);
     strong_cam_ref->SetPitch(-35.f);
-    engine.GetSunLight().lock()->Shadows = false;  // no shadows
-    setup_fpp_fly(editor_cam, engine);
-    setup_file_menu(engine);
-    engine.AddToOnQuit([]() { turn_off_fly(); remove_file_menu(); });
+    archer_engine_interface.GetSunLight().lock()->Shadows = false;  // no shadows
+    setup_fpp_fly(editor_cam, archer_engine_interface);
+    setup_file_menu(archer_engine_interface);
+    archer_engine_interface.AddToOnQuit([]() { turn_off_fly(); remove_file_menu(); });
 
   }
 
-  return engine.Run();
+  return archer_engine_interface.Run();
 }
