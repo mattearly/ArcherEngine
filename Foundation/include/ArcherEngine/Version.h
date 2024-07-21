7936929d25cfa20e0355c671b9018c245f814096
Matthew Early <matthewjearly@gmail.com>
2024-07-21 15:23:15 -0400

    update nfd lib

--- Foundation/include/ArcherEngine/Version.h
+++ Foundation/include/ArcherEngine/Version.h
@@ -1,6 +1,6 @@
 #pragma once
 namespace AA {
 constexpr unsigned int ENGINEVERSIONMAJOR = 2;
-constexpr unsigned int ENGINEVERSIONMINOR = 1;
+constexpr unsigned int ENGINEVERSIONMINOR = 2;
 constexpr unsigned int ENGINEVERSIONPATCH = 0;
 }
